5621f0795124a2554e1af06d796b37c5aebcd0db
raoxikui <raoxikui@qq.com>
2025-08-18 12:23:34 +0800

    优化

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        {{ env('APP_NAME') }}，从这里开始出发...
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
