ebfadfb9b8d6ab93451d016ce6a52f02decdd3ef
raoxikui <raoxikui@qq.com>
2026-01-21 15:17:09 +0800

    优化

--- public/index.php
+++ public/index.php
@@ -13,11 +13,14 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-\Swow\Coroutine::run(function () {
+$coroutine = \Swow\Coroutine::run(function () {
     // Bootstrap Laravel and handle the request...
     /** @var Application $app */
     $app = require_once __DIR__.'/../bootstrap/app.php';
 
     $app->handleRequest(Request::capture());
 });
+
+$coroutine->resume();
+
 \Swow\Sync\waitAll();
