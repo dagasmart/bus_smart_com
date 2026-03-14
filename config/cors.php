b0e851c6d11d979afac6d755a018ade5ae9d916c
raoxikui <raoxikui@qq.com>
2026-03-14 15:45:01 +0800

    优化

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,20 @@ return [
     |
     */
 
-    'paths' => ['admin-api/*', 'school-api/*', 'master-api/*', 'api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['admin-api/*', 'school-api/*', 'master-api/*', 'api/*', 'sanctum/csrf-cookie'], // 指定哪些路由需要应用 CORS 配置
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'], // 允许的方法，'*' 表示所有方法都允许
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'], // 允许的源，'*' 表示所有源都允许，生产环境中应指定具体源，例如：['https://example.com']
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [], // 使用正则表达式匹配源地址
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], // 允许的头部信息，'*' 表示所有头部都允许
 
-    'exposed_headers' => [],
+    'exposed_headers' => [], // 暴露的头部信息，例如：['X-My-Custom-Header']
 
-    'max_age' => 0,
+    'max_age' => 0, // 预检请求的结果（即选项请求的响应）可以被缓存多久，单位为秒。0 表示不缓存。
 
-    'supports_credentials' => false,
+    'supports_credentials' => false, // 是否支持用户凭证，例如 cookies 或 HTTP 认证。默认是 false。如果设置为 true，则 allowed_origins 不应为 '*'。
 
 ];
